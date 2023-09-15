d3939166cc854a40df48d45e0623c66076f66afd
Bryan Vera <brayanandresveragar@gmail.com>
2023-09-14 19:03:17 -0500

    :recycle: Mejore el nombre de las variables

--- index.js
+++ index.js
@@ -1,76 +1,106 @@
 //Snipet to select the html elements
 const $ = (selector) => document.querySelector(selector);
 
-// Info logo icons
-const email = $('.navbar-email');
+// Info Iconos con interaccion
+const correoBarraNavegacion = $('.navbar-email');
 const hamburguerMenu = $('.header_menu');
-const shoppingCart = $('.logo_shopping-cart');
+const botonShoppingCart = $('.logo_shopping-cart');
+const productDetailCloseIcon = $('.product-detail-close')
 
-// Info section
+// Info Elementos a modificar
 const desktopMenu = $('.desktop-menu');
 const mobileMenu = $('.mobile-menu');
-const shoppingSection = $('#shoppingCartContainer');
-const mainContainer = $('.main-container');
-const cardContainer = $('.cards-container');
+const productsInMyShoppingCart = $('#shoppingCartContainer');
+const productDetailSection = $('#productDetail');
+const containerGalleryProducts = $('.main-container');
+const galleryProducts = $('.cards-container');
 
+//? Add Event Listeners
+correoBarraNavegacion.addEventListener('click', openProfile);
+hamburguerMenu.addEventListener('click', openMenu);
+botonShoppingCart.addEventListener('click',  openShoppingCar);
+containerGalleryProducts.addEventListener('click', () => {
+    reset(productsInMyShoppingCart, mobileMenu)
+});
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside)
+
+//? Variables
 const productList = [];
 
-productList.push ({
-    nombre:'Bike',
-    precio: 12700,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-});
-productList.push ({
-    nombre:'Bicycle helmet',
-    precio: 1200,
-    image: 'https://assets.specialized.com/i/specialized/60821-104_HLMT_ALIGN-II-HLMT-MIPS-CE-BLK-BLKREFL-S-M_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto'
-});
-productList.push ({
-    nombre:'Bicycle helmet',
-    precio: 1600,
-    image: 'https://m.media-amazon.com/images/I/61eExL-rIAL._AC_SL1001_.jpg'
-});
-productList.push ({
-    nombre:'Bicycle helmet',
-    precio: 1500,
-    image: 'https://assets.specialized.com/i/specialized/60822-140_HLMT_CHAMONIX-HLMT-MIPS-CE-MRN-M-L_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto'
-});
-productList.push ({
-    nombre:'Seat',
-    precio: 300,
-    image: 'https://m.media-amazon.com/images/I/61e+sZ9rgNL._AC_SL1500_.jpg'
-});
-productList.push ({
-    nombre:'Tennis Montain Bike',
-    precio: 2200,
-    image: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/8ea578f6c07847fca2d0ac85011d7f1f_9366/Tenis_para_Mountain_Bike_Five_Ten_Freerider_Negro_FW2835_01_standard.jpg'
-});
-productList.push ({
-    nombre:'Sunglasses',
-    precio: 800,
-    image: 'https://cdn.siroko.com/s/files/1/1220/6874/products/gafas-siroko-tech-k3s-london-lateral/1200x/crop_center.jpg?v=1635209602'
-});
-productList.push ({
-    nombre:'Sunglasses',
-    precio: 600,
-    image: 'https://cdn.siroko.com/s/files/1/1220/6874/products/siroko-tech-k3s-clearfog-lente-antiniebla-frontal/1200x/crop_center.jpg?v=1635209603'
-});
-productList.push ({
-    nombre:'Bicycle seat bag',
-    precio: 876,
-    image: 'https://m.media-amazon.com/images/I/81k2Gmal+VL._AC_SL1500_.jpg'
-}); 
+function ingresarProducto(nombre,precio,imagen) {
+    const producto = {
+        nombre: nombre,
+        precio: precio,
+        image: imagen
+    }
+    productList.push(producto);
+}
 
-console.table(productList);
+ingresarProducto(
+    'Bike',
+    12700, 
+    'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+);
+ingresarProducto(
+    'Tennis Montain Bike',
+    2200,
+    'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/8ea578f6c07847fca2d0ac85011d7f1f_9366/Tenis_para_Mountain_Bike_Five_Ten_Freerider_Negro_FW2835_01_standard.jpg'
+);
+ingresarProducto(
+    'Bike',
+    12700, 
+    'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+);
+ingresarProducto(
+    'Tennis Montain Bike',
+    2200,
+    'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/8ea578f6c07847fca2d0ac85011d7f1f_9366/Tenis_para_Mountain_Bike_Five_Ten_Freerider_Negro_FW2835_01_standard.jpg'
+);
+ingresarProducto(
+    'Bike',
+    12700, 
+    'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+);
+ingresarProducto(
+    'Tennis Montain Bike',
+    2200,
+    'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/8ea578f6c07847fca2d0ac85011d7f1f_9366/Tenis_para_Mountain_Bike_Five_Ten_Freerider_Negro_FW2835_01_standard.jpg'
+);
+ingresarProducto(
+    'Bike',
+    12700, 
+    'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+);
+ingresarProducto(
+    'Tennis Montain Bike',
+    2200,
+    'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/8ea578f6c07847fca2d0ac85011d7f1f_9366/Tenis_para_Mountain_Bike_Five_Ten_Freerider_Negro_FW2835_01_standard.jpg'
+);
+ingresarProducto(
+    'Bike',
+    12700, 
+    'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+);
+ingresarProducto(
+    'Tennis Montain Bike',
+    2200,
+    'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/8ea578f6c07847fca2d0ac85011d7f1f_9366/Tenis_para_Mountain_Bike_Five_Ten_Freerider_Negro_FW2835_01_standard.jpg'
+);
+ingresarProducto(
+    'Bike',
+    12700, 
+    'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+);
+ingresarProducto(
+    'Tennis Montain Bike',
+    2200,
+    'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/8ea578f6c07847fca2d0ac85011d7f1f_9366/Tenis_para_Mountain_Bike_Five_Ten_Freerider_Negro_FW2835_01_standard.jpg'
+);
 
-email.addEventListener('click', openProfile);
-hamburguerMenu.addEventListener('click', openMenu);
-shoppingCart.addEventListener('click',  openShoppingCar);
-mainContainer.addEventListener('click', reset);
-
-function reset() {
-    shoppingSection.classList.add('inactive');
-    mobileMenu.classList.remove('showMenu');
+// * Esta funcion me ayuda a ocultar los elementos de menu cuando hay otro menu abierto
+function reset(elementoParaEsconder,elementoParaMostrar) {
+    elementoParaEsconder.classList.add('inactive');
+    elementoParaMostrar.classList.remove('inactive');
 }
 
 // ? Function to show and hide elements
@@ -78,27 +108,39 @@ function openProfile() {
     desktopMenu.classList.toggle('inactive');
 }
 
-function openMenu() {
-    const isShoppingSectionOpen = !shoppingSection.classList.contains('inactive');
+function openShoppingCar() {
 
-    if(isShoppingSectionOpen) {
-        shoppingSection.classList.add('inactive');
+    //* Validando que productDetailSection este abierto si ese es el caso entonces va a llamar a la fucnion reset donde se esconde el detail section y se muestra el productsInMyShoppingCart
+
+    if(!productDetailSection.classList.contains('inactive')){
+        reset(productDetailSection, productsInMyShoppingCart)
+    }else{
+        productsInMyShoppingCart.classList.toggle('inactive');
     }
 
-    mobileMenu.classList.toggle('showMenu');
-}
-
-function openShoppingCar() {
     const isMobileMenuOpen = !mobileMenu.classList.contains('inactive');
 
     if(isMobileMenuOpen) {
         mobileMenu.classList.remove('showMenu');
     }
-
-
-    shoppingSection.classList.toggle('inactive');
 }
 
+function openMenu() {
+    const isproductsInMyShoppingCartOpen = !productsInMyShoppingCart.classList.contains('inactive');
+
+    const isproductDetailSectionOpen = !productDetailSection.classList.contains('inactive');
+
+    if(isproductsInMyShoppingCartOpen) {
+        productsInMyShoppingCart.classList.add('inactive');
+    }else if(isproductDetailSectionOpen){
+        reset(productDetailSection, mobileMenu)
+        mobileMenu.classList.toggle('showMenu');
+    }else{
+        mobileMenu.classList.toggle('showMenu');
+    }
+}
+
+//* Renderizacion de los productos de HTML con JavaScript
 function productRender(array) {
     
     for (product of array){
@@ -107,6 +149,7 @@ function productRender(array) {
     
         const productImage = document.createElement('img');
         productImage.setAttribute('src', product.image)
+        productImage.addEventListener('click', openProductDetailAside);
     
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
@@ -139,8 +182,16 @@ function productRender(array) {
         productCard.append(productImage, productInfo);
     
     
-        cardContainer.appendChild(productCard);
+        galleryProducts.appendChild(productCard);
     }   
+}
+
+function openProductDetailAside (){
+    productDetailSection.classList.remove('inactive');
+}
+
+function closeProductDetailAside() {
+    productDetailSection.classList.add('inactive');
 }
 
 productRender(productList);
